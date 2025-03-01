707bda4ba6cd2b33ce0c83533b34dab4c82d1838
brancifortnick <brancifortnick@yahoo.com>
2025-03-01 01:56:10 -0500

    employee completed i think---> seed attempt

--- app/api/employee_routes.py
+++ app/api/employee_routes.py
@@ -1,18 +1,27 @@
 from flask import Flask, Blueprint, jsonify,request,render_template
 from flask_login import login_required
 from app.models import User,db, Employee
-
+from wtforms import FlaskForm
+from app.forms.employee_form import EmployeeForm
 employee_routes = Blueprint('employees', __name__)
 
 @employee_routes.route('/submit', methods=['POST'])
 def submit_choice():
-    form = EmployeeForm(data=request.json)
-    if form.validate():
-        new_choice = Employee(option=form.option.data, optional_text=form.optional_text.data, shift_worked=form.shift_worked.data)
+    data = request.get_json()  # Get JSON data from the request
+    form = EmployeeForm(data=data)  # Pass data to the form
+    
+    if form.validate_on_submit():  # Check form validation
+        new_choice = Employee(
+            option=form.option.data,
+            optional_text=form.optional_text.data,
+            shift_worked=form.shift_worked.data
+        )
         db.session.add(new_choice)
         db.session.commit()
         return jsonify(new_choice.to_dict()), 201
+    
     return jsonify({'errors': form.errors}), 400
+
 
 @employee_routes.route('/', methods=['GET'])
 def get_choices():
--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,9 +1,10 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
-
+import employee from './employee'
 const rootReducer = combineReducers({
   session,
+  employee,
 });
 
 
